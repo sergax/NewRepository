7390f4686294ebeedababd910a1c0607c772c5b5
sergax <serey.aksenchenko@gmail.com>
2022-01-06 01:42:33 +0200

    updating master(streamAPI)

---
+++ src/Stepik/streamAPI/MinMaxValues.java
@@ -0,0 +1,49 @@
+package Stepik.streamAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/*
+Напишите метод, находящий в стриме минимальный и максимальный элементы в соответствии порядком, заданным Comparator'ом.
+
+Найденные минимальный и максимальный элементы передайте в minMaxConsumer следующим образом:
+
+minMaxConsumer.accept(min, max);
+
+Если стрим не содержит элементов, то вызовите
+
+minMaxConsumer.accept(null, null);
+ */
+public class MinMaxValues {
+
+    public static void main(String[] args) throws Exception {
+        Stream<Integer> stream = new ArrayList<>(Arrays.asList(1, 2, 3, 5, 8, 13, 21)).stream();
+
+        findMinMax(
+                stream,
+                Integer::compareTo,
+                (x, y) -> System.out.printf("min: %s, max: %s%n", x, y)
+        );
+
+        stream.close();
+    }
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        // your implementation here
+
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
+        if (!list.isEmpty()) {
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        } else {
+            minMaxConsumer.accept(null, null);
+        }
+    }
+}
